6300dc83579cc9030e9b6f080bc961bd270f96d6
Josh McSavaney <mcsaucy@csh.rit.edu>
2020-08-13 22:04:22 -0400

    Fix a broken line continuation

--- mk_ipxe_script.sh
+++ mk_ipxe_script.sh
@@ -9,7 +9,7 @@ set base-url http://stable.release.flatcar-linux.net/amd64-usr/current
 kernel \${base-url}/flatcar_production_pxe.vmlinuz \
     initrd=flatcar_production_pxe_image.cpio.gz \
     flatcar.first_boot=1 \
-    sshkey=\"$KEY\"
+    sshkey=\"$KEY\" \
     ignition.config.url=${IGN_PATH:-https://raw.githubusercontent.com/mcsaucy/ephemeros/master/ignition.ign}
 initrd \${base-url}/flatcar_production_pxe_image.cpio.gz
 boot"
